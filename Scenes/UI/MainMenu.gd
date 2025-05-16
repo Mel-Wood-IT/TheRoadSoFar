75e94acdebec1f60ed325fa9f671a19278ab483b
JontySN <jonty.southcombe@gmail.com>
2025-05-16 21:49:03 +1200

    test

--- Scenes/UI/MainMenu.gd
+++ Scenes/UI/MainMenu.gd
@@ -8,7 +8,7 @@ func _ready():
 
 # Button to play the game
 func _on_PlayBtn_pressed():
-	get_tree().change_scene("res://Level One.tscn")
+	pass # Replace with function body.
 
 # Button to open about page
 func _on_AboutBtn_pressed():
