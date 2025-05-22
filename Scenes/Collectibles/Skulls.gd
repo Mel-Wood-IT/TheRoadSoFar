3e8a4c6f39eacab16987c748348170d11625eaaa
Mel <melissa@foxit.co.nz>
2025-05-22 20:35:43 +1200

    More stuff

--- Scenes/Collectibles/Skulls.gd
+++ Scenes/Collectibles/Skulls.gd
@@ -4,7 +4,10 @@ func _on_SkullWhite_body_entered(body):
 	if body.name == "Player":
 		queue_free()
 
-
 func _on_SkullYelBlood_body_entered(body):
+	if body.name == "Player":
+		queue_free()
+
+func _on_SkullWhiBlood_body_entered(body):
 	if body.name == "Player":
 		queue_free()
